693a4aeaae17423cedb62a9d1fb117c4c0f39fbd
PavelS07 <62809253+PavelS07@users.noreply.github.com>
2022-03-16 14:58:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const dateNow = document.querySelector('.date-now');
+const dateYesterday = document.querySelector('.date-yesterday');
+dateNow.textContent = data.now;
+dateYesterday.textContent = data.yesterday;
